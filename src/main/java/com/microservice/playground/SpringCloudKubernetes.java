f36f1db6af66cb9629901daaa56833296638319e
David Bingham <david.r.bingham@gmail.com>
2018-02-14 13:48:18 +0000

    Updating POM

---
+++ src/main/java/com/microservice/playground/SpringCloudKubernetes.java
@@ -0,0 +1,13 @@
+package com.microservice.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudKubernetes {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudKubernetes.class, args);
+    }
+
+}
